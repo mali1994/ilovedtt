b023e81b330d106ec482bf128a922ef238d483d2
unknown <m_li@foxmail.com>
2019-05-31 20:24:20 +0800

    2019年5月31日20:24:18

--- file/functions.js
+++ file/functions.js
@@ -7,6 +7,7 @@ var $win = $(window);
 var clientWidth = $win.width();
 var clientHeight = $win.height();
 
+
 $(window).resize(function() {
     var newWidth = $win.width();
     var newHeight = $win.height();
